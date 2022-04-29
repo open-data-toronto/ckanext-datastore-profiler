b14607c59c85075d7da412aa39676aaa200d0912
mackeynichols <mackey.nichols@gmail.com>
2022-04-29 11:30:48 -0400

    basic, tested Profiler class to read from and write to CKAN API

--- datastore_profiler.py
+++ datastore_profiler.py
@@ -1,61 +1,85 @@
-# datastore_profiler.py - for each datastore resource, create a summary of each of its attributes
+# datastore_profiler.py - for input package name's datastore resources, add profile object to each datastore field metadata
 
+from unicodedata import numeric
 import requests
 import json
-import utils.datastore_profiler_utils as datastore_profiler_utils
-
-def check_assert(bool, out):
-    if not bool:
-        print(out)
-
-# PROD Open Data Portal API endpoint base url
-url = "https://ckanadmin0.intra.prod-toronto.ca/"
-
-# init output
-output = []
-
-# init requests sessions avoiding the use of a proxy
-session = requests.Session()
-session.trust_env = False
-
-# get list of all package names from source and target, and sort them
-package_names = json.loads( session.get( url + "api/action/package_list").text)["result"]
-
-# for each package, check if its resources are datastore
-for package_name in ["bodysafe"]:#package_names:
-    package = json.loads( session.get( url + "api/action/package_show?id=" + package_name).text)["result"]
-    # if this package has any datastore resources, look them up in detail
-    for resource in package["resources"]:
-        if resource["datastore_active"]:
-            # get attributes, length of datastore resource
-            datastore_resource_summary = json.loads( session.get( url + "api/action/datastore_search?limit=0&id=" + resource["id"]).text)["result"]
-            data = {field["id"]: {"type": field["type"], "data": [] } for field in datastore_resource_summary["fields"] }
-            # logic for smaller datastore resources
-            if datastore_resource_summary["total"] < 32000:
-                datastore_resource = json.loads( session.get( url + "api/action/datastore_search?limit=32000&id=" + resource["id"]).text)["result"]
-                # for each record, stick into the "data" object
-                for object in datastore_resource["records"]:
-                    for field in list(object.keys()):
-                        data[ field ]["data"].append( object[field] )
+from utils import numericstatistics, datestatistics, stringstatistics
+import pandas
 
 
-            # TODO: chunking logic for larger datastore resources
-    
-            # once data is put in to the data object, let's put it through our utils
-            for field in datastore_resource_summary["fields"]:
+class Profiler:
+    def __init__(self, package_name, env="dev"):
 
-                # for strings, we'll run string analyses
-                print(field["id"])
-                print(data[field["id"]]["type"])
+        
+        # Open Data Portal API endpoint base urls
+        ckan_urls = {
+            "dev": "https://ckanadmin0.intra.dev-toronto.ca/",
+            "qa": "https://ckanadmin0.intra.qa-toronto.ca/",
+            "prod": "https://ckanadmin0.intra.prod-toronto.ca/",
+        }
+        assert env in ckan_urls.keys(), "input env is not a valid environment - choose from " + str(list(ckan_urls))
+        self.url = ckan_urls[env]
 
-                if data[field["id"]]["type"] == "text":
-                    print(datastore_profiler_utils.mask_count(data[field["id"]]["data"]))
-                    print(datastore_profiler_utils.unique_count(data[field["id"]]["data"]))
+        # init requests sessions avoiding the use of a proxy
+        self.session = requests.Session()
+        self.session.trust_env = False
 
-                if data[field["id"]]["type"] in ["int", "int4", "float", "float8"]:
-                    print(datastore_profiler_utils.numeric_count(data[field["id"]]["data"]))
+        # init package name var
+        assert package_name in json.loads( self.session.get( self.url + "api/action/package_list").text)["result"], "Input package name is not in " + self.url
+        self.package_name = package_name
 
-                if data[field["id"]]["type"] == "timestamp":
-                    print( datastore_profiler_utils.timestamp_count(data[field["id"]]["data"]) )
+
+    def get_package_metadata(self):
+        return json.loads( self.session.get( self.url + "api/action/package_show?id=" + self.package_name).text)["result"]
+
+    def get_datastore_resource_fields_metadata(self):
+        output = {}
+        # for each datastore resource in self.package_name, return {resource_id: datastore fields metadata}
+        for resource in self.get_package_metadata()["resources"]:
+            if resource["datastore_active"] in [True]:
+                # get attributes, length of datastore resource, and fields metadata
+                datastore_resource_summary = json.loads( self.session.get( self.url + "api/action/datastore_search?limit=0&id=" + resource["id"]).text)["result"]
+                output[ resource["id"] ] = datastore_resource_summary["fields"]
+
+        return output
+
+    def profile_datastore_resource(self):
+        all_fields_metadata = self.get_datastore_resource_fields_metadata()
+        for resource_id in all_fields_metadata.keys():
+            # for each resource
+            fields_metadata = all_fields_metadata[resource_id]
             
+            # dump data into pandas dataframe
+            df = pandas.read_csv( self.url + "datastore/dump/" + resource_id + "?format=csv", na_filter=False )
             
+            # for each field, add appropriate profile to the metadata aobject
+            for i in range(len(fields_metadata)):
+                fieldname = fields_metadata[i]["id"]
+                if fieldname == "_id": # we dont want to touch '_id' - we remove it later in this method
+                    continue
+                
+                field_data = df[fieldname].tolist()
+
+                if fields_metadata[i]["type"] in ["int", "int4", "float8"]:
+                    fields_metadata[i]["info"]["profile"] = numericstatistics.NumericStatistics().numeric_count(field_data)
+                elif fields_metadata[i]["type"] in ["date", "timestamp"]:
+                    fields_metadata[i]["info"]["profile"] = datestatistics.DateStatistics().date_count(field_data)
+                else:
+                    fields_metadata[i]["info"]["profile"] = stringstatistics.StringStatistics().execute(field_data)
+
+            # get rid of _id column - CKAN doesnt allow us to insert columns with that name
+            for i in range(len(fields_metadata)):
+                fieldname = fields_metadata[i]["id"]
+                if fieldname == "_id":
+                    fields_metadata.pop(i)
+                    print("removing _id")
+                    break
+            
+            # write edited resource metadata into ckan
+            headers = {"Authorization": ""}
+            result = json.loads( self.session.post( self.url + "api/action/datastore_create", json={"resource_id": resource_id, "fields": fields_metadata, "force":True}, headers=headers ).text)
+            assert result["success"], "Failed to update profiles for " + resource_id
+            
+
+if __name__ == "__main__":
+    Profiler("bodysafe").profile_datastore_resource()
--- utils/stringstatistics.py
+++ utils/stringstatistics.py
@@ -8,7 +8,7 @@ class StringStatistics:
         This class is a Collection of methods that compute statistics for strings 
     """
 
-    def unique_count(input):
+    def unique_count(self, input):
         """
             Method to count unique strings
         Args:
@@ -16,7 +16,6 @@ class StringStatistics:
         Returns:
             output (dict): output dict - each key is a string from the input, and the number of times it appears as the value                           
         """
-        assert all(isinstance(x, str) for x in input if x), "Input to unique_count() must be all strings or null"
 
         # init output
         output = {}
@@ -36,7 +35,7 @@ class StringStatistics:
         return {k: v for k, v in sorted(output.items(), key=lambda item: item[1], reverse=True)}
 
 
-    def mask_count(input):
+    def mask_count(self, input):
         """
             Method to compute the count of masks for string data 
             Args:
@@ -45,13 +44,11 @@ class StringStatistics:
                 output (dict): each key a mask string, and each value how many times it occurs. Each mask has L for letter, D for digit, with each non-alphanumeric shown as is            
         """
         
-        assert all(isinstance(x, str) for x in input if x), "Input to mask_count() must be all strings or null"
-
         # init output
         output = {}
 
         # for each string
-        for string in [x for x in input if x]:
+        for string in [str(x) for x in input if x]:
             pattern = re.sub( '[a-zA-Z]', "L", string )
             pattern = re.sub( '[0-9]', "D", pattern )
 
@@ -68,7 +65,7 @@ class StringStatistics:
         return {k: v for k, v in sorted(output.items(), key=lambda item: item[1], reverse=True)}
 
     
-    def word_count(input):
+    def word_count(self, input):
         
         """
                 Method that splits each string by spaces and calculates statistics on each resulting "word"
@@ -79,11 +76,8 @@ class StringStatistics:
         """
         output = {}
 
-        # make sure inputs are datetimes or nulls
-        assert [isinstance(value, str) for value in input if value], "Input to word_count can only be string or null"
-
         # remove nulls from working data
-        working_data = [value for value in input if value]
+        working_data = [str(value) for value in input if value]
 
         # split each string by spaces, and make sure all the resulting "words" are in a single list
         working_words = " ".join(working_data).split(" ")
@@ -109,3 +103,10 @@ class StringStatistics:
                 "min_string_length": min( [len(words) for words in working_data] ),
                 "max_string_length": max( [len(words) for words in working_data] )
             }
+
+    def execute(self, input):
+        output = self.word_count(input)
+        output["unique_count"] = self.unique_count(input)
+        output["mask_count"] = self.mask_count(input)
+        return output
+
