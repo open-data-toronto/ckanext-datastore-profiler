f1c66f710cd5d99bd3fe3cf3d375b04796cd35d2
tummala-hareesh <tummala.ahri@gmail.com>
2022-05-04 13:40:13 -0400

    [code] visualize stat results as tables - HTML

--- datastore_profiler.py
+++ datastore_profiler.py
@@ -1,5 +1,5 @@
 # datastore_profiler.py - for input package name's datastore resources, add profile object to each datastore field metadata
-
+from fileinput import filename
 import requests
 import json
 from utils import numericstatistics, datestatistics, stringstatistics
@@ -38,7 +38,7 @@ class Profiler:
 
     def profile_datastore_resources_dump(self):
         """ 
-            Loads all the OpenData data sources as a single dump file and runs the profiler on every data sources
+            Loads all the OpenData data resources as a single dump file and runs the profiler on every data sources
         """
         all_fields_metadata = self.get_datastore_resource_fields_metadata()
         for resource_id in all_fields_metadata.keys():
@@ -75,6 +75,76 @@ class Profiler:
             result = json.loads( self.session.post( self.url + "api/action/datastore_create", json={"resource_id": resource_id, "fields": fields_metadata, "force":True}, headers=headers ).text)
             assert result["success"], "Failed to update profiles for " + resource_id
             
+    def visualize_datastore_resource(self, resource_id):
+        """ 
+            Run profiler on a data resource with resource_id
+        """
+        # dump data into pandas dataframe
+        fields_json = pandas.read_json( self.url + "api/action/datastore_search?id=" + resource_id + "&limit=0")['result']['fields']
+        
+        # Flatten data
+        df = pandas.json_normalize(fields_json, max_level=1)
+
+        # Initialize dicts
+        dict_numerics  = dict()
+        dict_datetimes = dict()
+        dict_strings   = dict()
+
+        # for each field, add appropriate profile to the metadata aobject
+        for i in range(df.shape[0]):
+
+            # Set fieldname
+            field_name = df.loc[i, 'id']
+
+            # Skip row if id = _id
+            if field_name == "_id": # we dont want to touch '_id' 
+                continue
+            else:
+                field_type = df.loc[i, 'type']
+                field_profile = df.loc[i, 'info.profile']                
+                print(i, field_name, field_type)
+
+                if (field_type in ["int", "int4", "float8"]):
+                    dict_numerics[field_name] = field_profile         # append to dict_numerics 
+                elif (field_type in ["date", "timestamp"]):
+                    dict_datetimes[field_name] = field_profile        # append to dict_datetimes 
+                else:
+                    dict_strings[field_name] = field_profile          # append to dict_strings 
+
+        # Convert dicts into dataframes
+        df_numerics  = pandas.DataFrame(dict_numerics)
+        df_datetimes = pandas.DataFrame(dict_datetimes)
+        df_strings   = pandas.DataFrame(dict_strings)
+
+        # Option-1: Save numerics stats as html table (for displaying on City's Page)
+        df_numerics.T.to_html('html/table_numerics_stats.html')
+
+        # Option-1: Save datetimes stats as html table (for displaying on City's Page)
+        df_datetimes.T.to_html('html/table_datetimes_stats.html')
+
+        # Option-1: Save strings stats as html table (for displaying on City's Page)
+        df_strings.T.to_html('html/table_strings_stats.html')
+
+        print('>> Completed - HTMLs')
+
+        # # get rid of _id column - CKAN doesnt allow us to insert columns with that name
+        # for i in range(len(fields_metadata)):
+        #     fieldname = fields_metadata[i]["id"]
+        #     if fieldname == "_id":
+        #         fields_metadata.pop(i)
+        #         break
+        
+        # # write edited resource metadata into ckan
+        # headers = {"Authorization": self.apikey}
+        # result = json.loads( self.session.post( self.url + "api/action/datastore_create", json={"resource_id": resource_id, "fields": fields_metadata, "force":True}, headers=headers ).text)
+        # assert result["success"], "Failed to update profiles for " + resource_id
+         
+
 
 if __name__ == "__main__":
-    Profiler("bodysafe", "https://ckanadmin0.intra.dev-toronto.ca/", "" ).profile_datastore_resources_dump()
+    # Data Resource Dump
+    #Profiler("bodysafe", "https://ckanadmin0.intra.dev-toronto.ca/", "" ).profile_datastore_resources_dump()
+
+    # Visualize Single Data Resource 
+    resource_id = 'f9f84603-973c-43eb-bed0-a0ec26a2652b'
+    Profiler("bodysafe", "https://ckanadmin0.intra.dev-toronto.ca/", "" ).visualize_datastore_resource(resource_id)
