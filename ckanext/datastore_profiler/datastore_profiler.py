f3140418cd72b920d6a256c2314f6df7b7887a05
reza-ghasemzadeh <rgs1375@gmail.com>
2022-11-18 15:41:31 +0000

    another silly change

--- ckanext/datastore_profiler/datastore_profiler.py
+++ ckanext/datastore_profiler/datastore_profiler.py
@@ -163,4 +163,4 @@ def datastore_create_hook(original_datastore_create, context, data_dict):
     tk.get_action("package_patch")(context, {"id": package["name"], "tags": package_tags })
 
     # some codes to extend datastore profiler abilities
-    print("end")
+    assert 5 < 3, "Five is greater than three!"
